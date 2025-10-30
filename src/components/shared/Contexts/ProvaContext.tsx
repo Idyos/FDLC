06ec01f10c52b7047d73475b005aad540f25a06d
JaviGauxachs <javier@xalera.cat>
2025-10-30 13:43:27 +0100

    clear prova

--- src/components/shared/Contexts/ProvaContext.tsx
+++ src/components/shared/Contexts/ProvaContext.tsx
@@ -1,12 +1,15 @@
+// useProvaStore.ts
 import { create } from "zustand";
 import { ProvaInfo } from "@/interfaces/interfaces";
 
 interface ProvaState {
   prova: ProvaInfo | null;
   setProva: (prova: ProvaInfo | null) => void;
+  clearProva: () => void;
 }
 
 export const useProvaStore = create<ProvaState>((set) => ({
   prova: null,
   setProva: (prova) => set({ prova }),
+  clearProva: () => set({ prova: null }),
 }));
